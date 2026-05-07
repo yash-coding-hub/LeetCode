35a50f42a3350d182f51f6616c7104f04f5e7258
yash-coding-hub <sharmayash7100@gmail.com>
2026-05-07 23:51:30 +0530

    Time: 0 ms (100%), Space: 8.8 MB (81.47%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+
+        int i = a.length()-1;
+        int j = b.length()-1;
+
+        string count = "";
+
+        int carry = 0;
+
+        while(i >= 0 || j >= 0){
+
+            int sum = carry;
+
+            if(i >= 0){
+                sum += (a[i]-'0');
+                i--;
+            }
+
+            if(j >= 0){
+                sum += (b[j]-'0');
+                j--;
+            }
+
+            count.push_back((sum % 2) + '0');
+
+            carry = sum / 2;
+        }
+
+        if(carry){
+            count.push_back(carry + '0');
+        }
+
+        reverse(count.begin(), count.end());
+
+        return count;
+    }
+};
