911d834b4835bfcaa2df7e6fe9c270f3b22c9e37
yash-coding-hub <sharmayash7100@gmail.com>
2026-05-08 23:36:04 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.48%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int y = 11;
+        while(y > 10){
+            int rem = num%10;
+            int quo = num/10;
+            y = rem + quo;
+            if(y>10){
+                num = y;
+                continue;
+            }
+            else{
+                break;
+            }
+        }
+        if(y == 10){
+            return 1;
+        }
+        else{
+        return y;
+        }
+    }
+};
