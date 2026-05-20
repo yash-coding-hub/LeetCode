8dcf2a0cbaff22a7737851981a13f938d9738f41
yash-coding-hub <sharmayash7100@gmail.com>
2026-05-21 00:28:37 +0530

    Time: 0 ms (100%), Space: 8.2 MB (41.16%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int> result;
+
+        for(int i=1; i<=n; i++){
+            if(n % i == 0){
+                result.push_back(i);
+            }
+        }
+
+        if(result.size() < k){
+            return -1;
+        }
+
+        else{
+            return result[k-1];
+        }
+    }
+};
