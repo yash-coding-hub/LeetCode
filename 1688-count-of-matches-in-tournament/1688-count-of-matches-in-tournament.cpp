70a195cf4fbc4792d11194e312b7f1fe4dcaa64c
Yash Sharma <sharmayash7100@gmail.com>
2026-07-02 00:06:48 +0530

    Time: 0 ms (100%), Space: 7.8 MB (92.04%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        // int count = 0;
+        // int matches = 0;
+        
+
+        // while(n > 1){
+        //     if(n % 2 != 0){
+        //         n = n - 1;
+        //         count++;
+        //     }
+
+        //     matches = matches + n / 2;
+
+        //     n = count + (n / 2);
+        //     count = 0;
+        // }
+
+        // return matches;
+
+        return n-1;
+    }
+};
